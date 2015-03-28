1a1447dbd965d1b7bc5a1aa29cafa6e45db76cd5
Sven <sven.stolz0380@googlemail.com>
2015-03-29 00:16:35 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,10 +15,10 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 		
 		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_name('h1').text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 		
-		inputbox =- self.browser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 		
 		inputbox.send_keys('Buy peacock feathers')
@@ -26,7 +27,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+		'Couldn\'t find row with \'Buy peacock feathers\'')
 		
 		self.fail('Finish the test!')
 
