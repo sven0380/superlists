c0d056a1f96c3783ac80dc46d9283618e97740eb
Sven <sven.stolz0380@googlemail.com>
2015-03-29 22:47:30 +0800

    refactored functional test

--- functional_tests.py
+++ functional_tests.py
@@ -32,7 +32,6 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-
 	
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
