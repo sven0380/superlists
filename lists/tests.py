ef1d26f012b912c0d4d06e7e5183fffff172e361
Sven <sven.stolz0380@googlemail.com>
2015-03-29 22:44:42 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -18,14 +18,25 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
-		
-	def test_homepage_can_save_a_post_request(self):
+	
+	def do_post_request_with_new_item(self, item_text):
 		request = HttpRequest()
 		request.method = 'POST'
 		request.POST['item_text'] = 'A new list item'
-		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
-		
+		return home_page(request)
+	
+	def test_homepage_can_save_a_post_request(self):
+		item_text = 'A new list item'
+		response = self.do_post_request_with_new_item(item_text)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+	
+	def test_homepage_can_redirect_after_a_post_request(self):
+		response = self.do_post_request_with_new_item('irrelevant name')
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+	
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
 		first_item.text = 'The first list item (like ever)'
@@ -42,3 +53,16 @@ class HomePageTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first list item (like ever)')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		
+	def test_homepage_only_saves_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+		
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='my item 1')
+		Item.objects.create(text='my item 2')
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertIn('my item 1', response.content.decode())
+		self.assertIn('my item 2', response.content.decode())
