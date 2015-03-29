f8171572fd32b4c289febd4225aa5c10c30af7ef
Sven <sven.stolz0380@googlemail.com>
2015-03-29 20:49:54 +0800

    add saving of first to-do entry

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 	
@@ -20,16 +21,21 @@ class NewVisitorTest(unittest.TestCase):
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-		
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very
+		# methodical)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-		'Couldn\'t find row with \'Buy peacock feathers\'')
-		
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', 
+			[row.text for row in rows])
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
