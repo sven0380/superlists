e6f429db56a058103f67ee63a282fed39d88af7a
Sven <sven.stolz0380@googlemail.com>
2015-03-31 23:10:38 +0800

    Extended functional test that new browser window has no old items

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,6 +34,30 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 	
+		#The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.fail('Finish the test!')
+
+		# Now a new user, Francis, comes along to the site.
+		
+		## We use a new browser session to make sure that no information
+		## of Edith's is coming through from cookies etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		
+		# Francis visits the home page. There is no sign of Edith's
+		# list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+		
+		# Francis starts a new list by entering a new item. He
+		# is less interesting than Edith...
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		
+		# Satisfied, they both go back to sleep
+		
+		
