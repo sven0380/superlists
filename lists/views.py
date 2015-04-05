12730fd78049c77d021c8e402d696acfa2e783ab
Sven <sven.stolz0380@googlemail.com>
2015-04-05 13:06:11 +0800

    refactored templates

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,9 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST.get('item_text', ''))
         return redirect('/')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
