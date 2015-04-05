65f2dad5fa98345980942d4e762a21d22540362b
Sven <sven.stolz0380@googlemail.com>
2015-04-05 13:19:28 +0800

    some other fixes - now it's on status as on page 91

--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
